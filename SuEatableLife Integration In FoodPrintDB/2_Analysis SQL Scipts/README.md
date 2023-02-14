cc8a35ee08fbe1ebe12b4d1ef7a6a7eb9aaec8de
AlbertoGigantelliGabriele <gigantellialberto@gmail.com>
2023-02-14 19:50:05 +0100

    Delete typo directory

--- SuEatableLife Integration In FoodPrintDB/2_Analysis SQL Scipts/README.md
+++
@@ -1,3 +0,0 @@
-# 2_Analysis SQL Scipts
-
-The execution of "coverage_and_frequency_ingredients.sql" requires the execution of "0_original_food_print_ingredients_table.sql".
--- SuEatableLife Integration In FoodPrintDB/2_Analysis SQL Scipts/most_and_least_sustainable_recipes.sql
+++
@@ -1,3 +0,0 @@
-select * from food_print.recipes where static_score is not null order by static_score desc LIMIT 10;
-
-select * from food_print.recipes where static_score is not null order by static_score asc LIMIT 10;
