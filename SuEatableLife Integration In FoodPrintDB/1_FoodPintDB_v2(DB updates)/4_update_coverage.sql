6f98a1385f1996ad420ae5674edd1c73ef9dbe7d
aiacovazzi <ar.iacovazzi@gmail.com>
2023-02-21 21:29:27 +0100

    fixes

--- SuEatableLife Integration In FoodPrintDB/1_FoodPintDB_v2(DB updates)/4_update_coverage.sql
+++ SuEatableLife Integration In FoodPrintDB/1_FoodPintDB_v2(DB updates)/4_update_coverage.sql
@@ -2,7 +2,7 @@ update recipes set trust_cfp = null, trust_wfp = null where 1;
 
 #coverage of recipes whose ingredients has cfp
 with cfp_cov as(
-select d.*, (d.n_with_footprint*100/d.total) as recipe_coverage from (
+select d.*, (d.n_with_footprint/d.total) as recipe_coverage from (
 select r.recipe_id, r.title, SUM(CASE 
              WHEN i.carbon_foot_print is not null THEN 1
              ELSE 0
@@ -13,7 +13,7 @@ select r.recipe_id, r.title, SUM(CASE
            END) AS n_without_footprint, count(i.ingredient_id) as total  from recipes r join ingredients_recipes ir on r.recipe_id = ir.recipe_id join ingredients i on i.ingredient_id = ir.ingredient_id   
 		   group by r.recipe_id order by n_without_footprint desc, n_with_footprint asc) d order by recipe_coverage desc), 
 wfp_cov as(
-select d.*, (d.n_with_footprint*100/d.total) as recipe_coverage from (
+select d.*, (d.n_with_footprint/d.total) as recipe_coverage from (
 select r.recipe_id, r.title, SUM(CASE 
              WHEN i.water_foot_print is not null THEN 1
              ELSE 0
